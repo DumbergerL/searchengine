6234d3bebbc4cfd85c1dd07152fc0c7e147725c3
Lukas Dumberger <lukas.dumberger@dumbergerl.de>
2020-03-17 11:01:46 +0100

    add test data to database

--- sql/searchengine.sql
+++ sql/searchengine.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 17. Mrz 2020 um 10:58
+-- Erstellungszeit: 17. Mrz 2020 um 11:00
 -- Server-Version: 10.4.11-MariaDB
 -- PHP-Version: 7.2.26
 
@@ -34,6 +34,14 @@ CREATE TABLE `links` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Daten für Tabelle `links`
+--
+
+INSERT INTO `links` (`id`, `link`, `updated_at`) VALUES
+(1, 'http://www.dhbw-heidenheim.de', '2020-03-17 09:59:36'),
+(2, 'http://www.facebook.com', '2020-03-17 09:59:50');
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +52,15 @@ CREATE TABLE `words` (
   `id` int(11) NOT NULL,
   `word` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `words`
+--
+
+INSERT INTO `words` (`id`, `word`) VALUES
+(1, 'DHBW'),
+(3, 'Netzwerk'),
+(2, 'Student');
 
 -- --------------------------------------------------------
 
@@ -56,6 +73,16 @@ CREATE TABLE `word_links` (
   `link_id` int(11) NOT NULL,
   `word_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Daten für Tabelle `word_links`
+--
+
+INSERT INTO `word_links` (`id`, `link_id`, `word_id`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(3, 2, 3),
+(4, 2, 1);
 
 --
 -- Indizes der exportierten Tabellen
@@ -88,19 +115,19 @@ ALTER TABLE `word_links`
 -- AUTO_INCREMENT für Tabelle `links`
 --
 ALTER TABLE `links`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT für Tabelle `words`
 --
 ALTER TABLE `words`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT für Tabelle `word_links`
 --
 ALTER TABLE `word_links`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
