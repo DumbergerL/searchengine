124cabc7ee8f992b2247b292d7ee0dda0600cf29
Patrick Kratzer <patrickk295@gmail.com>
2020-03-19 12:31:29 +0100

    move text parts into several php files

---
+++ public/content/search.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Patrick Kratzer
+ * Date: 19.03.2020
+ * Time: 11:37
+ */
+
+/**
+ * @return string
+ */
+function getSearchAddSiteContent()
+{
+    return "<div id='content_container' class='full-height'>
+        <div id='search_container' class='input-group mb-3'>
+            <form method='get'>
+                <div class='form-group'>
+                    <input type='text' class='form-control' placeholder='Search term' name='query' required>
+                    <input type='hidden' name='page' value='result'>
+                </div>
+                <button class='btn btn-success' type='submit'><i class='fa fa-search'></i> Run search</button>
+            </form>
+        </div>
+        <div id='add_site_container' class='input-group mb-3' style='display: none'>
+            <form method='post'>
+                <div class='form-group'>
+                    <input type='text' class='form-control' placeholder='Site/URL to be added' required>
+                </div>
+                <button class='btn btn-success' type='submit' style='vertical-align: middle'><i class='fa fa-search'></i>Add URL</button>
+            </form>
+        </div>
+    </div>";
+}
